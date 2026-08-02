28a5944b66309d0abcb9728dc6c08ac15d500ef2
Rupam2024 <163507186+Rupam2024@users.noreply.github.com>
2026-08-02 10:26:10 +0530

    Implement load_embeddings function
    Added a function to load embeddings using the specified model.

--- vectorstore/retriever.py
+++ vectorstore/retriever.py
@@ -6,3 +6,12 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 CHROMA_DB_PATH = "chroma_db"
 
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
+# Load Embeddings
+def load_embeddings():
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=EMBEDDING_MODEL
+    )
+
+    return embeddings
