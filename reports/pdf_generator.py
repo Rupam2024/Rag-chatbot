803763d69230353f14d40457b4d7a528bed2d5db
Rupam2024 <163507186+Rupam2024@users.noreply.github.com>
2026-09-17 18:58:07 +0530

    Implement PDFGenerator class for drug reports
    Added a PDFGenerator class with a method to create drug reports.

--- reports/pdf_generator.py
+++ reports/pdf_generator.py
@@ -13,3 +13,37 @@ from reportlab.lib import colors
 
 from datetime import datetime
 import os
+
+# PDF Generator
+
+class PDFGenerator:
+
+    def __init__(self):
+
+        self.styles = getSampleStyleSheet()
+
+    # Create Single Drug Report
+    
+
+    def create_drug_report(
+        self,
+        drug_data,
+        output_file
+    ):
+
+        doc = SimpleDocTemplate(
+            output_file
+        )
+
+        elements = []
+
+        title = Paragraph(
+            "Drug Information Report",
+            self.styles["Title"]
+        )
+
+        elements.append(title)
+
+        elements.append(
+            Spacer(1, 12)
+        )
