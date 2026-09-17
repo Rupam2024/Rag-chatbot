149b517f5eae087fb1deed6f4a004f2bf7bbdcf1
Rupam2024 <163507186+Rupam2024@users.noreply.github.com>
2026-09-17 18:56:43 +0530

    Add initial PDF generator implementation

---
+++ reports/pdf_generator.py
@@ -0,0 +1,15 @@
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    PageBreak
+)
+
+from reportlab.lib.styles import (
+    getSampleStyleSheet
+)
+
+from reportlab.lib import colors
+
+from datetime import datetime
+import os
